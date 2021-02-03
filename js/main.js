afbe6556ce3c4c916417794c68070273b3d3ccf1
taylanbildik <34001728+taylanbildik@users.noreply.github.com>
2021-02-04 00:51:23 +0300

    fix

--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('../sw-sayfa.js')
-      .then(reg => console.log('Service Worker: Registered (Pages)'))
-      .catch(err => console.log(`Service Worker: Error: ${err}`));
-  });
-}
